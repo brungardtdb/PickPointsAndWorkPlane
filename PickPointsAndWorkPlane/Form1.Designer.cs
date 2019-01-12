64e11734120c8907970dc1819c9a7f393cc1ff0c
David Brungardt <brungardtdb@gmail.com>
2019-01-12 12:33:39 -0700

    Initial commit

---
+++ PickPointsAndWorkPlane/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace PickPointsAndWorkPlane
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPickPoints = new System.Windows.Forms.Button();
+            this.btnPickPart = new System.Windows.Forms.Button();
+            this.btnGlobal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPickPoints
+            // 
+            this.btnPickPoints.Location = new System.Drawing.Point(12, 12);
+            this.btnPickPoints.Name = "btnPickPoints";
+            this.btnPickPoints.Size = new System.Drawing.Size(75, 23);
+            this.btnPickPoints.TabIndex = 0;
+            this.btnPickPoints.Text = "Pick Points";
+            this.btnPickPoints.UseVisualStyleBackColor = true;
+            this.btnPickPoints.Click += new System.EventHandler(this.btnPickPoints_Click);
+            // 
+            // btnPickPart
+            // 
+            this.btnPickPart.Location = new System.Drawing.Point(12, 41);
+            this.btnPickPart.Name = "btnPickPart";
+            this.btnPickPart.Size = new System.Drawing.Size(75, 23);
+            this.btnPickPart.TabIndex = 1;
+            this.btnPickPart.Text = "Pick Part";
+            this.btnPickPart.UseVisualStyleBackColor = true;
+            this.btnPickPart.Click += new System.EventHandler(this.btnPickPart_Click);
+            // 
+            // btnGlobal
+            // 
+            this.btnGlobal.Location = new System.Drawing.Point(12, 70);
+            this.btnGlobal.Name = "btnGlobal";
+            this.btnGlobal.Size = new System.Drawing.Size(75, 23);
+            this.btnGlobal.TabIndex = 2;
+            this.btnGlobal.Text = "Global";
+            this.btnGlobal.UseVisualStyleBackColor = true;
+            this.btnGlobal.Click += new System.EventHandler(this.btnGlobal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(211, 104);
+            this.Controls.Add(this.btnGlobal);
+            this.Controls.Add(this.btnPickPart);
+            this.Controls.Add(this.btnPickPoints);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPickPoints;
+        private System.Windows.Forms.Button btnPickPart;
+        private System.Windows.Forms.Button btnGlobal;
+    }
+}
+
---
+++ PickPointsAndWorkPlane/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Tekla Structures Namespace References
+using Tekla.Structures;
+using Tekla.Structures.Model;
+using T3D = Tekla.Structures.Geometry3d;
+using TSMUI = Tekla.Structures.Model.UI;
+using System.Collections;
+
+namespace PickPointsAndWorkPlane
+{
+    public partial class Form1 : Form
+    {
+        // new model declaration
+        Model Model;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+            try
+            {
+                // new model instantiation
+                Model = new Model();
+            }
+            catch 
+            {
+                // if user is not connected to tekla model
+                MessageBox.Show("Please connect to Tekla Structures");
+            }
+        }
+
+        private void btnPickPoints_Click(object sender, EventArgs e)
+        {
+            T3D.Point firstPoint = null; // first point picked by user
+            T3D.Point secondPoint = null; // second point picked by user
+
+            TSMUI.Picker picker = new TSMUI.Picker(); // picker for user to pick points
+
+            try
+            {
+                // store picked points in an ArrayList
+                ArrayList pickedPoints =  picker.PickPoints(Tekla.Structures.Model.UI.Picker.PickPointEnum.PICK_TWO_POINTS);
+
+                firstPoint = pickedPoints[0] as T3D.Point; // first picked point
+                secondPoint = pickedPoints[1] as T3D.Point; // second picked point
+            }
+            catch // if user cancels without selecting points
+            {
+                // set both points to null
+                firstPoint = null;
+                secondPoint = null;
+
+                // inform user that first and second points weren't chosen
+                MessageBox.Show("First and second points were not chosen.");
+            }
+            finally
+            {
+                // if first and second points aren't null
+                if (firstPoint != null && secondPoint != null)
+                {
+                    // subtract distance between two points
+                    T3D.Vector XVector = new T3D.Vector(secondPoint.X - firstPoint.X, secondPoint.Y - firstPoint.Y, secondPoint.Z - firstPoint.Z);
+                    T3D.Vector YVector = new T3D.Vector(new T3D.Vector(0, 0, -1)); 
+                    
+                    // use selected points to set work plane
+                    Model.GetWorkPlaneHandler().SetCurrentTransformationPlane(new TransformationPlane(firstPoint, XVector, YVector));
+
+                    // commit changes to model
+                    Model.CommitChanges();
+                }
+            }
+        }
+
+        private void btnPickPart_Click(object sender, EventArgs e)
+        {
+            // declaration for object user will select
+            ModelObject pickedObject = null;
+
+            // New picker for user interface
+            TSMUI.Picker picker = new TSMUI.Picker();
+
+            try
+            {
+                // if user picks an object
+                pickedObject = picker.PickObject(TSMUI.Picker.PickObjectEnum.PICK_ONE_OBJECT);
+            }
+            catch
+            {
+                // if user interrupts
+                pickedObject = null;
+                MessageBox.Show("No object was selected.");
+            }
+            finally
+            {
+                if (pickedObject != null)
+                {
+                    // get coordinate system for object
+                    T3D.CoordinateSystem objectSystem = pickedObject.GetCoordinateSystem();
+
+                    // set transformation plane to picked object coordinate system
+                    Model.GetWorkPlaneHandler().SetCurrentTransformationPlane(new TransformationPlane(objectSystem));
+
+                    Model.CommitChanges();
+                }
+            }
+
+
+        }
+
+        private void btnGlobal_Click(object sender, EventArgs e)
+        {
+            // set transformation plane to default transformation plane using empty constructor
+            Model.GetWorkPlaneHandler().SetCurrentTransformationPlane(new TransformationPlane());
+
+            Model.CommitChanges();
+        }
+    }
+}
